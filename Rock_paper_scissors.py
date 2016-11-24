4c65d1a8b35b43ea070869ae53e22b6cc3b39a43
JB2120 <jdbraithwaite@hotmail.com>
2016-11-24 12:04:22 +0000

    recommend the update as it has code now

--- Rock_paper_scissors.py
+++ Rock_paper_scissors.py
@@ -1,1 +1,15 @@
 #here goes the program goes
+player = input('player 1 or player 2:')
+if player == '1':
+    played = input('enter r p or s:')
+    data = open("rps data.txt",'r+')
+    data.seek(0,0)
+    data.writelines(played)
+    data.close()
+if player == '2':
+    played = input('enter r p or s:')
+    data = open("rps data.txt",'r+')
+    data.seek(1,0)
+    data.writelines(played)
+    data.close()
+    
